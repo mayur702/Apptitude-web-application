f0a18f052a6f8ece96355c1c8261144c2edc3e10
mayur702 <mayurmagar702@gmail.com>
2025-04-05 07:04:05 +0530

    Terraform add

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,7 +17,7 @@ resource "aws_ecs_cluster" "apptitude_cluster" {
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+  name = "ecsTaskExecutionRoles"
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
